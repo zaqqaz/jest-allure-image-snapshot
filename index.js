e898b052d3c94084d2a0b6b95bb402e4b5442fb2
Denis Artyuhovich <i@denis.by>
2018-06-21 18:02:39 +0300

    Initialization

---
+++ index.js
@@ -0,0 +1,21 @@
+const { toMatchImageSnapshot: jestToMatchImageSnapshot } = require("jest-image-snapshot");
+
+const fs = require("fs");
+
+function toMatchImageSnapshot(...args) {
+    /* tslint:disable */
+    const result = jestToMatchImageSnapshot.apply(this, args);
+    /* tslint:enable */
+
+    if (!result.pass) {
+        const message = result.message();
+        const fileName = /\/(.*).png/gm.exec(message)[0];
+        if (fileName && fs.existsSync(fileName)) {
+            const buffer = fs.readFileSync(fileName);
+            reporter.addAttachment("Image Snapshot Diff", buffer, "image/png");
+        }
+    }
+    return result;
+}
+
+expect.extend({ toMatchImageSnapshot });
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Jest-Allure-Image-Snapshot
+
+Plugin for image comparison use [jest-image-snapshots](https://github.com/americanexpress/jest-image-snapshot/blob/master/examples/image-reporter.js) and beateaful allure reports using [jest-allure](https://github.com/zaqqaz/jest-allure-image-snapshot)
+
+# Installation
+
+```
+yarn add -D jest-allure-image-snapshot
+```
+or
+
+```
+npm install --save-dev jest-allure-image-snapshot
+```
