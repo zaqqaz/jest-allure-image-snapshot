1d088471233053dd2bd0ebc03c14fd21de21d415
Denis Artyuhovich <i@denis.by>
2018-07-26 17:35:26 +0300

    Fix custom config

--- src/imageSnapshot/imageResizer.js
+++ src/imageSnapshot/imageResizer.js
@@ -1,28 +1,28 @@
 const { PNG } = require('pngjs');
 
 const fillSizeDifference = (width, height) => (image) => {
-  const inArea = (x, y) => y > height || x > width;
-  for (let y = 0; y < image.height; y++) {
-    for (let x = 0; x < image.width; x++) {
-      if (inArea(x, y)) {
-        const idx = ((image.width * y) + x) << 2;
-        image.data[idx] = 0;
-        image.data[idx + 1] = 0;
-        image.data[idx + 2] = 0;
-        image.data[idx + 3] = 64;
-      }
+    const inArea = (x, y) => y > height || x > width;
+    for (let y = 0; y < image.height; y++) {
+        for (let x = 0; x < image.width; x++) {
+            if (inArea(x, y)) {
+                const idx = ((image.width * y) + x) << 2;
+                image.data[idx] = 0;
+                image.data[idx + 1] = 0;
+                image.data[idx + 2] = 0;
+                image.data[idx + 3] = 64;
+            }
+        }
     }
-  }
-  return image;
+    return image;
 };
 
-export const createImageResizer = (width, height) => (source) => {
+const createImageResizer = (width, height) => (source) => {
     const resized = new PNG({ width, height, fill: true });
     PNG.bitblt(source, resized, 0, 0, source.width, source.height, 0, 0);
     return resized;
 };
 
-export const alignImagesToSameSize = (first, second) => {
+const alignImagesToSameSize = (first, second) => {
     const {
         width: firstWidth,
         height: firstHeight,
@@ -46,4 +46,8 @@ export const alignImagesToSameSize = (first, second) => {
         fillSizeDifference(firstWidth, firstHeight)(resizedFirst),
         fillSizeDifference(secondWidth, secondHeight)(resizedSecond),
     ];
+};
+
+module.exports = {
+    alignImagesToSameSize,
 };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
-const { toMatchImageSnapshot: jestToMatchImageSnapshot } = require("./imageSnapshot");
+const { configureToMatchImageSnapshot } = require("./imageSnapshot");
 
 const fs = require("fs");
 
-function toMatchImageSnapshot(...args) {
-    const result = jestToMatchImageSnapshot.apply(this, args);
+function registerAllureImageSnapshot(config) {
+    config.customSnapshotsDir = config.customSnapshotsDir || "__image_snapshots__";
+    const jestToMatchImageSnapshot = configureToMatchImageSnapshot(config);
 
-    if (!result.pass) {
-        const message = result.message();
-        const fileName = /\/(.*).png/gm.exec(message)[0];
-        if (fileName && fs.existsSync(fileName)) {
-            const buffer = fs.readFileSync(fileName);
-            reporter.addAttachment("Image Snapshot Diff", buffer, "image/png");
+    function toMatchImageSnapshot(...args) {
+        const result = jestToMatchImageSnapshot.apply(this, args);
+
+        if (!result.pass) {
+            const message = result.message();
+            const fileName = config.customSnapshotsDir + /\/(.*).png/gm.exec(message)[0];
+
+            if (fileName && fs.existsSync(fileName)) {
+                const buffer = fs.readFileSync(fileName);
+                reporter.addAttachment("Image Snapshot Diff", buffer, "image/png");
+            }
         }
+        return result;
     }
-    return result;
-}
 
-function registerAllureImageSnapshot() {
     expect.extend({ toMatchImageSnapshot });
 }
 
